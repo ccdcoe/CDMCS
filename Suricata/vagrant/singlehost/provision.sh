fca3ee7f58bd050f36e3629f36e7d82ca0387058
Markus Kont <markuskont@gmail.com>
2017-12-24 22:29:29 +0200

    influx + telegraf deployed

--- Suricata/vagrant/singlehost/provision.sh
+++ Suricata/vagrant/singlehost/provision.sh
@@ -16,6 +16,8 @@ ELA="elasticsearch-6.1.1.deb"
 # 6.1.1 index management was broken for me
 KIBANA="kibana-6.0.1-amd64.deb"
 LOGSTASH="logstash-6.1.1.deb"
+INFLUX="influxdb_1.4.2_amd64.deb"
+TELEGRAF="telegraf_1.5.0-1_amd64.deb"
 
 # basic OS config
 start=$(date)
@@ -45,13 +47,56 @@ mkdir -p /vagrant/pkgs
 # basic software
 #apt-get update > /dev/null && apt-get install curl htop vim tmux > /dev/null
 
+# suricata
+install_suricata_from_ppa(){
+  add-apt-repository ppa:oisf/suricata-stable > /dev/null 2>&1 \
+  && apt-get update > /dev/null \
+  && apt-get install -y suricata > /dev/null
+}
+echo "Provisioning SURICATA"
+suricata -V || install_suricata_from_ppa
+
+[[ -f /etc/suricata/rules/scirius.rules ]] || touch /etc/suricata/rules/scirius.rules
+if $DEBUG ; then ip addr show; fi
+systemctl stop suricata
+FILE=/etc/suricata/suricata.yaml
+grep "Amstelredamme" $FILE || cat >> $FILE <<EOF
+# Amstelredamme added by vagrant
+af-packet:
+  - interface: enp0s3
+    cluster-id: 98
+    cluster-type: cluster_flow
+    defrag: yes
+  - interface: enp0s8
+    cluster-id: 97
+    cluster-type: cluster_flow
+    defrag: yes
+  - interface: eth0
+    cluster-id: 96
+    cluster-type: cluster_flow
+    defrag: yes
+  - interface: eth1
+    cluster-id: 95
+    cluster-type: cluster_flow
+    defrag: yes
+default-rule-path: /etc/suricata/rules
+rule-files:
+ - scirius.rules
+sensor-name: suricata
+EOF
+
+touch  /etc/suricata/threshold.config
+if $DEBUG ; then suricata -T -vvv; fi
+
+check_service suricata
+
 # java
 install_oracle_java() {
   echo "Installing oracle Java"
   echo 'oracle-java8-installer shared/accepted-oracle-license-v1-1 boolean true' | debconf-set-selections \
   && add-apt-repository ppa:webupd8team/java > /dev/null 2>&1 \
   && apt-get update > /dev/null \
-  && DEBIAN_FRONTEND=noninteractive apt-get -y install oracle-java8-installer > /dev/null
+  && apt-get -y install oracle-java8-installer > /dev/null
 }
 echo "Provisioning JAVA"
 java -version || install_oracle_java
@@ -78,40 +123,10 @@ EOF
 
 check_service kibana
 
-# suricata
-install_suricata_from_ppa(){
-  add-apt-repository ppa:oisf/suricata-stable > /dev/null 2>&1 \
-  && apt-get update > /dev/null \
-  && apt-get install -y suricata > /dev/null
-}
-echo "Provisioning SURICATA"
-suricata -V || install_suricata_from_ppa
+# set up default index pattern
+curl -ss -XPUT localhost:9200/.kibana/doc/index-pattern:a1571060-e8e2-11e7-9cf4-db76e233e72b -d @/vagrant/kibana-index-pattern.json -H'Content-Type: application/json'
+curl -ss -XPUT localhost:9200/.kibana/doc/config:6.0.1 -d @/vagrant/kibana-index-pattern-config.json -H'Content-Type: application/json'
 
-[[ -f /etc/suricata/rules/scirius.rules ]] || touch /etc/suricata/rules/scirius.rules
-if $DEBUG ; then ip addr show; fi
-systemctl stop suricata
-FILE=/etc/suricata/suricata.yaml
-grep "Amstelredamme" $FILE || cat >> $FILE <<EOF
-# Amstelredamme added by vagrant
-af-packet:
-  - interface: enp0s3
-    cluster-id: 98
-    cluster-type: cluster_flow
-    defrag: yes
-  - interface: enp0s8
-    cluster-id: 97
-    cluster-type: cluster_flow
-    defrag: yes
-default-rule-path: /etc/suricata/rules
-rule-files:
- - scirius.rules
-sensor-name: suricata
-EOF
-
-touch  /etc/suricata/threshold.config
-if $DEBUG ; then suricata -T -vvv; fi
-
-check_service suricata
 # logstash
 echo "Provisioning LOGSTASH"
 cd $PKGDIR
@@ -142,7 +157,59 @@ output {
 }
 EOF
 curl -ss -XPUT localhost:9200/_template/default -d @/vagrant/elastic-default-template.json -H'Content-Type: application/json'
-curl -ss -XPUT localhost:9200/.kibana/doc/index-pattern:a1571060-e8e2-11e7-9cf4-db76e233e72b -d @/vagrant/kibana-index-pattern.json -H'Content-Type: application/json'
-curl -ss -XPUT localhost:9200/.kibana/doc/config:6.0.1 -d @/vagrant/kibana-index-pattern-config.json -H'Content-Type: application/json'
 chown root:logstash /var/log/suricata/eve.json
 check_service logstash
+
+# influx
+echo "Provisioning INFLUXDB"
+cd $PKGDIR
+[[ -f $INFLUX ]] || wget $WGET_PARAMS https://dl.influxdata.com/influxdb/releases/$INFLUX -O $INFLUX
+dpkg -i $INFLUX > /dev/null 2>&1
+systemctl stop influxdb.service
+
+check_service influxdb
+
+# telegraf
+echo "Provisioning TELEGRAF"
+cd $PKGDIR
+[[ -f $TELEGRAF ]] || wget $WGET_PARAMS https://dl.influxdata.com/telegraf/releases/$TELEGRAF -O $TELEGRAF
+dpkg -i $TELEGRAF > /dev/null 2>&1
+
+systemctl stop telegraf.service
+grep "Amstelredamme" $FILE || cat > $FILE <<EOF
+[global_tags]
+  provision = "Amstelredamme"
+
+[agent]
+  hostname = "syslog.yellow.ex"
+  omit_hostname = false
+  interval = "1s"
+  round_interval = true
+  metric_buffer_limit = 1000
+  flush_buffer_when_full = true
+  collection_jitter = "0s"
+  flush_interval = "60s"
+  flush_jitter = "10s"
+  debug = false
+  quiet = true
+
+[[outputs.influxdb]]
+  database  = "telegraf"
+  urls  = ["http://localhost:8086"]
+
+[[inputs.cpu]]
+  percpu = true
+  totalcpu = true
+[[inputs.disk]]
+  ignore_fs = ["tmpfs", "devtmpfs"]
+[[inputs.diskio]]
+[[inputs.kernel]]
+[[inputs.mem]]
+[[inputs.net]]
+[[inputs.netstat]]
+[[inputs.processes]]
+[[inputs.swap]]
+[[inputs.system]]
+EOF
+
+check_service telegraf
