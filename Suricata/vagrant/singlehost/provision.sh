fe805248badf5de64f9e61d924ce66ea232fd435
Markus Kont <markuskont@gmail.com>
2017-12-24 17:57:33 +0200

    wip

--- Suricata/vagrant/singlehost/provision.sh
+++ Suricata/vagrant/singlehost/provision.sh
@@ -1,9 +1,19 @@
-# script params
-# set versions here
+check_service(){
+  systemctl daemon-reload
+  systemctl is-enabled $1.service 2>/dev/null | grep "disabled" && systemctl enable $1.service
+  systemctl status $1.service | egrep  "inactive" && systemctl start $1.service
+  if $DEBUG ; then systemctl status $1.service ; fi
+}
 
-# SURICATA="4.0.3"
-DEBUG=true
+# params
+DEBUG=false
 PROXY=http://192.168.10.1:3128
+PKGDIR=/vagrant/pkgs
+WGET_PARAMS="-4"
+
+# versions
+ELA=6.1.1
+#SURICATA=4.0.3
 
 # basic OS config
 start=$(date)
@@ -28,11 +38,41 @@ source /etc/profile
 FILE=/etc/apt/apt.conf.d/99force-ipv4
 [[ -f $FILE ]] ||  echo 'Acquire::ForceIPv4 "true";' | sudo tee $FILE
 export DEBIAN_FRONTEND=noninteractive
+mkdir -p /vagrant/pkgs
 
-# install suricata but check if already exists
-# add-apt-repository assumes ubuntu
-suricata -V || bash -c "add-apt-repository ppa:oisf/suricata-stable && apt-get update && apt-get install -y suricata && suricata -V"
-if $debug ; then ip addr show; fi
+# basic software
+apt-get update > /dev/null && apt-get install curl htop vim tmux > /dev/null
+
+# java
+install_oracle_java() {
+  echo 'oracle-java8-installer shared/accepted-oracle-license-v1-1 boolean true' | debconf-set-selections \
+  && add-apt-repository ppa:webupd8team/java > /dev/null 2>&1 \
+  && apt-get update > /dev/null \
+  && DEBIAN_FRONTEND=noninteractive apt-get -y install oracle-java8-installer > /dev/null
+}
+echo "Provisioning JAVA"
+java -version || install_oracle_java
+
+# elastic
+echo "Provisioning ELASTICSEARCH"
+FILE=$PKGDIR/elasticsearch-$ELA.deb
+SHA=$FILE.sha512
+[[ -f $FILE ]] || wget $WGET_PARAMS https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$ELA.deb -O $FILE
+[[ -f $FILE.sha512 ]] || wget $WGET_PARAMS https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$ELA.deb.sha512 -O $SHA
+cd $PKGDIR && shasum -a 512 -c $SHA && dpkg -i $FILE
+
+check_service elasticsearch
+
+# suricata
+install_suricata_from_ppa(){
+  add-apt-repository ppa:oisf/suricata-stable > /dev/null 2>&1 \
+  && apt-get update > /dev/null \
+  && apt-get install -y suricata > /dev/null
+}
+echo "Provisioning SURICATA"
+suricata -V || install_suricata_from_ppa
+
+#if $DEBUG ; then ip addr show; fi
 systemctl stop suricata
 FILE=/etc/suricata/suricata.yaml
 grep "Amstelredamme" $FILE || cat >> $FILE <<EOF
@@ -53,9 +93,6 @@ sensor-name: suricata
 EOF
 
 touch  /etc/suricata/threshold.config
-if $debug ; then suricata -T -vvv; fi
+#if $DEBUG ; then suricata -T -vvv; fi
 
-systemctl daemon-reload
-systemctl is-enabled suricata.service 2>/dev/null | grep disabled && systemctl enable suricata.service
-systemctl status suricata.service | egrep  "inactive" && systemctl start suricata.service
-if $debug ; then systemctl status suricata.service ; fi
+check_service suricata
