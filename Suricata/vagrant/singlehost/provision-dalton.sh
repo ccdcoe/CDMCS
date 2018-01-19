0c2ff84b8328fcdd008e6b1d4e34c40caa723d73
Markus Kont <markuskont@gmail.com>
2018-01-19 17:58:17 +0200

    dalton, yay

--- Suricata/vagrant/singlehost/provision-dalton.sh
+++ Suricata/vagrant/singlehost/provision-dalton.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND=noninteractive
+
 echo "Provisioning DALTON"
+apt-get install -y docker-compose > /dev/null 2>&1
+
 cd /opt
 [[ -d dalton ]] || git clone https://github.com/secureworks/dalton.git
-
-apt-get install -f docker-compose
-
 cd dalton && sed -i 's/DALTON_EXTERNAL_PORT=80/DALTON_EXTERNAL_PORT=8087/g' .env
 docker-compose build && docker-compose up -d
