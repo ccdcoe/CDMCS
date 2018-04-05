b6352645aaafe197b3f5482401da37abb838c7dd
Markus Kont <markuskont@gmail.com>
2018-04-05 14:54:54 +0300

    build vagrant env done

--- Moloch/vagrant/build/buildMoloch.sh
+++ Moloch/vagrant/build/buildMoloch.sh
@@ -1,37 +1,27 @@
 #!/bin/bash
 #
-# it will build, install and configure Moloch
-#
+# it will build, install and configure Moloch as regular user
+# https://github.com/npm/npm/issues/19883
 
-if [ "$(id -u)" != "0" ]; then
-   echo "ERROR - This script must be run as root" 1>&2
-   exit 1
+VER=$1
+ME=$(whoami)
+
+if ! grep -q "moloch-$VER" $HOME/.bashrc; then
+  echo "export PATH=\"$PATH:$HOME/moloch-$VER/bin\"" >> $HOME/.bashrc
+fi
+if ! echo $PATH | grep -q "moloch-$VER"; then
+  export PATH="$PATH:$HOME/moloch-$VER/bin"
 fi
 
-start=$(date)
-echo $start >  /vagrant/provision.log
-echo "Installing prerequisite standard packages..."
-echo 'Acquire::ForceIPv4 "true";' | sudo tee /etc/apt/apt.conf.d/99force-ipv4
-export DEBIAN_FRONTEND=noninteractive
-apt-get -y install wget curl libpcre3-dev uuid-dev libmagic-dev pkg-config g++ flex bison zlib1g-dev libffi-dev gettext libgeoip-dev make libjson-perl libbz2-dev libwww-perl libpng-dev xz-utils libffi-dev >> /vagrant/provision.log 2>&1
-echo "installing node v4.6.0...."
-# see https://github.com/aol/moloch#building-viewer
-apt-get -y install python-minimal >> /vagrant/provision.log 2>&1
-cd /tmp
-wget -q -4 wget https://nodejs.org/dist/v4.6.0/node-v4.6.0-linux-x64.tar.xz
-tar -xf node-v4.6.0-linux-x64.tar.xz
-mv node-v4.6.0-linux-x64 /opt/
-ln -s /opt/node-v4.6.0-linux-x64/bin/node /usr/local/bin/node
-ln -s /opt/node-v4.6.0-linux-x64/lib/node_modules/npm/bin/npm-cli.js /usr/local/bin/npm
+echo "Pulling moloch"
+[[ -d $HOME/moloch-build ]] || git clone https://github.com/aol/moloch $HOME/moloch-build
+cd $HOME/moloch-build
+git checkout tags/$VER
 
-echo "getting moloch source..."
-cd /tmp
-wget -q -4 https://github.com/aol/moloch/archive/master.tar.gz
-tar -xf master.tar.gz
-echo "building moloch..."
-cd moloch-master/
-./easybutton-build.sh >> /vagrant/provision.log 2>&1
+echo "Building"
+./easybutton-build.sh -d $HOME/moloch-$VER >> /vagrant/provision.log 2>&1
+sudo chown -R $ME $HOME/moloch-$VER
+
+echo "Installing"
 make install >> /vagrant/provision.log 2>&1
-echo -en "enp0s3;enp0s8;\nno\nhttp://localhost:9200\ns2spassword\n" | make config >> /vagrant/provision.log 2>&1
-echo "testing moloch..."
-/data/moloch/bin/moloch-capture --nospi
+moloch-capture --help
---
+++ Moloch/vagrant/build/provision.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# it will set up system-wide prereqs to build and configure moloch as regular user
+#
+
+MOLOVER="v1.0.0-rc2"
+USER="vagrant"
+
+if [ "$(id -u)" != "0" ]; then
+   echo "ERROR - This script must be run as root" 1>&2
+   exit 1
+fi
+
+start=$(date)
+echo $start >  /vagrant/provision.log
+echo "Installing prerequisite packages..."
+echo 'Acquire::ForceIPv4 "true";' | sudo tee /etc/apt/apt.conf.d/99force-ipv4
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get -y install wget curl python-minimal libpcre3-dev uuid-dev libmagic-dev pkg-config g++ flex bison zlib1g-dev libffi-dev gettext libgeoip-dev make libjson-perl libbz2-dev libwww-perl libpng-dev xz-utils libffi-dev >> /vagrant/provision.log 2>&1
+
+echo "Provision moloch $MOLOVER as $USER"
+cp /vagrant/buildMoloch.sh /home/$USER/buildMoloch.sh && chown $USER /home/$USER/buildMoloch.sh
+time su -c "bash /home/$USER/buildMoloch.sh $MOLOVER" $USER
+
+echo "DONE :: start $start end $(date)"
