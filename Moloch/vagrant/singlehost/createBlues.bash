73ee20e9f679d29e7f4693563b8a45ee7b95445d
Hillar Aarelaid <hillar@aarelaid.net>
2018-05-07 15:41:16 +0300

    create blues

---
+++ Moloch/vagrant/singlehost/createBlues.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /data/moloch/viewer
+for T in $(seq 01 22); do
+  P=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+  echo ${T} ${P}
+  ../bin/node addUser.js "blue${T}" "Blue${T}" "${P}" --expression "country.src==${T}"
+  echo "${P}" > blue${T}.pwd
+done
