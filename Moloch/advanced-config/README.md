e087679dc312e422f9aa73bfb100d1e22c58e69d
Markus Kont <markus.kont@ccdcoe.org>
2019-04-22 17:54:17 +0000

    fun stuff

---
+++ Moloch/advanced-config/README.md
@@ -0,0 +1,227 @@
+# Optimizing the capture
+
+All credit goes to:
+  * https://github.com/pevma/SEPTun
+  * https://github.com/pevma/SEPTun-Mark-II
+
+Note that each network card has different capabilities. A lot of notes here simply cannot be done on a vagrant virtual machine.
+
+## **MUST-HAVE** - disable NIC offloading
+
+Modern NIC-s can offload a lot of stuff to hadrware. For example, packet checksumming. This is really good for hosting serices and routing. We don't care! We want the full packet! Otherwise, how can we properly reconstruct the session!
+
+```
+for iface in enp130s0f1 enp130s0f0 ; do
+  for i in rx tx tso ufo gso gro lro tx nocache copy sg txvlan rxvlan; do
+    ethtool -K $eth $i off 2>&1 > /dev/null;
+  done
+done
+```
+
+Note, moloch has likely already complained about this before. Sometimes it even exists during live mode when partial traffic is detected.
+
+## The RSS thing
+
+In short, if you are using multiple networking queues on your NIC, then your capture will have a bad time. And you may not even know about it.
+
+See:
+
+  * https://suricata.readthedocs.io/en/latest/performance/packet-capture.html?highlight=rss#rss
+  * https://github.com/pevma/SEPTun/blob/master/SEPTun.rst#common-misconceptions---part-1
+  * https://github.com/pevma/SEPTun/blob/master/SEPTun.rst#correct-nic-driver-with-correct-parameters
+  * https://github.com/pevma/SEPTun-Mark-II/blob/master/SEPTun-Mark-II.rst#setup-symmetric-hashing-on-the-nic
+
+tl;dr - all traffic is RX for capture box, but NIC decides which queue should handle the traffic based on 5-tuple. That decicion is asymmetric, so RX and TX may end up in different queues. That is bad, mkay.
+
+There are a few options:
+  * use a single NIC queue if amount of traffic and packet interrutps is not a problem;
+  * use a NIC that supports symmetric flow hasing AND patch your NIC driver and ethtool;
+  * use the [low entropy hash trick](http://www.ndsl.kaist.edu/~kyoungsoo/papers/TR-symRSS.pdf);
+  * buy an expensive capture card that does packet reordering in hardware;
+  * hipster tech that moloch does not currently support (looking at you ebpf and XDP);
+
+### Using a single NIC queue
+
+Check how many queues are actually used:
+
+```
+ethtool -l $iface
+```
+
+Then change that number to 1.
+
+```
+ethtool -L $iface combined 1
+```
+
+You should pin this thread to a specific core and make sure that nothing else is scheduled there (see cpu affinity in later secionts). You can see the interrupt handler id by grepping for interface name from this `/proc/interrupts`. Then under `/proc/irq`, look for that id and find the affinity from `smp_affinity_list` file (requires 3.someting kernel, otherwise use `smp_affinity` which is same thing in hex).
+
+```
+cat /proc/interrupts | grep $iface
+cat /proc/irq/$id/smp_affinity_list
+```
+
+You can pin irq hander to a specific thread. For example, if you want thread `1` to handle all interrupts for a specific NIC ring, you can do this:
+
+```
+echo 1 > /proc/irq/$id/smp_affinity_list
+```
+
+Note that all this work will be totally pointless unless you don't disable the irqbalaner daemon. It will override your configs.
+
+```
+systemctl stop irqbalaner.service
+systemctl disable irqbalaner.service
+```
+
+This method has a nasty limit - you only have one CPU core for handling all NIC interrupts. If that core is saturated, then new packets will no longer be picked up from ringbuffer by the kernel. In other words, you are burining one CPU thread and are limited to 1-2 million PPS, depending on CPU frequency (1 million on an average 2.4GHz XEON seems to be a valid roof). It may seem a lot, but average PPS for LS was ~200k with packet deduplication (x3 without), with 500k-700k peaks. That is already a 50-70% core saturation on peak traffic.
+
+## Low entropy hashing
+
+There is another way to handle this problem. Some smart people figured out that you can archieve symmteric hashing that also provides nearly perfect load balancing by replacing your NIC hash key. First, configure whatever number of queues you'd like. 
+
+```
+ethtool -L $iface combined 18
+ethtool -K $iface rxhash on
+ethtool -K $iface ntuple on
+```
+
+Then, we can override the default hash key with a low-entropy one that is essentially just a repeating bit pattern.
+
+```
+ethtool -X $iface hkey 6D:5A:6D:5A:6D:5A:6D:5A:6D:5A:6D:5A:6D:5A:6D:5A:6D:5A:6D:5A:6D:5A:6D:5A:6D:5A:6D:5A:6D:5A:6D:5A:6D:5A:6D:5A:6D:5A:6D:5A:6D:5A:6D:5A:6D:5A:6D:5A:6D:5A:6D:5A equal 18
+```
+
+And use a script to pin those irq handlers to specific threads. For example, if NUMA node our NIC is connected to has 20 cores, 0-9 and 20-29. This is also the reason for selecting 18 queues - few should be reserved for management and logging threads, otherwise number of queues should be equal to number of packet workers.
+
+```
+set_irq_affinity 1-9,21-29 $iface
+```
+
+This approach is less proven than others. Septun mark II article used ixgbe driver with 40bit hash key, but Intel x710 card uses a 52bit key instead. Since it's the repeating pattern that causes symmetric effect, then extending that pattern to fill 52 bits is quite simple, and the results seemed to be quite okay. But this observation is far from scientific. Also, a low-end x520-da2 with ixgbe driver did not allow me to change the hash key at all, so you are still at the mercy of your capture card features. This approach does let you to totally cheap out.
+
+## CPU affinity and NUMA
+
+But which cores do I want to isolate (i.e., what is NUMA)? Start by exploring your system topology.
+
+```
+apt-get install hwloc
+lstopo --logical --output-format txt
+```
+
+Do not trust what it says! Instead, ask from a tool meant to understand NUMA. Sometames odd numbered threads are numa 0 while even numbered are numa 1. Other times, 0-19 is 0 and 20-39 is 1. Other times, 0-9 is 0, 10-19 is 1, 20-29 is 0, 30-39 is 1. Depends on your hardware, kernel, etc.
+
+```
+numactl --hardware
+```
+
+Then start your processes and assign them to specific cores.
+
+```
+taskset -pc $core_num $pid
+```
+
+Or even better, start capture process with `numactl` instead, as it keeps all threads on correct node and even confines the process to local memory. The latter is a big deal - memory latency is the whole reason we do this!
+
+```
+numactl --cpunodebind=1 --membind=1 /data/moloch/bin/moloch-capture -c /data/moloch/etc/config.ini
+```
+
+We can also bind the process to the same NUMA node where our capture interface is located.
+
+```
+/usr/bin/numactl --cpunodebind=netdev:$iface --membind=netdev:$iface /data/moloch/bin/moloch-capture -c /data/moloch/etc/config.ini
+```
+
+Note that `numactl` still allows the individual threads to be scheduled on any available CPU thread on that NUMA node. You might want to find the process ID-s of those workers and pin them to specific threads. This is what I used during the exercise `psutil` in python is quite magical.
+
+```python
+#!/usr/bin/env python3
+
+import psutil
+import subprocess
+import re
+import sys
+import os.path
+
+def get_moloch_capture_parent():
+    procs = {p.pid: p.info for p in psutil.process_iter(attrs=['pid', 'name', 'username'])}
+    parent = {k: v for k, v in procs.items() if "moloch-capture" in v["name"]}
+    parent = list(parent.values())[0]["pid"]
+    parent = psutil.Process(pid=parent)
+    return parent
+
+def get_moloch_workers(parent):
+    workers = parent.threads()
+    workers = [w.id for w in workers]
+    workers = [psutil.Process(pid=p) for p in workers]
+    workers = [{"pid": p.pid, "name": p.name()} for p in workers]
+    return workers
+
+def get_numa_cores(node):
+    numa = subprocess.run(['numactl', '--hardware'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    numa = numa.split("\n")
+    numa = [v.split(":")[1].strip() for v in numa if "node {} cpus:".format(NODE) in v][0]
+    numa = numa.split()
+    numa = [int(v) for v in numa]
+    return numa
+
+NODE=0
+CAP_IFACE="$IFACE_EXT"
+
+if __name__ == "__main__":
+
+    numa = get_numa_cores(NODE)
+
+    intr_thread = numa[0]
+    cap_thread = numa[1]
+    worker_threads = numa[2:]
+
+    cap_pattern = re.compile("^moloch-(?:capture|simple|af\d+-\d+)$")
+    pkt_pattern = re.compile("^moloch-pkt\d+$")
+
+    parent = get_moloch_capture_parent()
+    workers = get_moloch_workers(parent)
+
+    cap_threads = [t for t in workers if cap_pattern.match(t["name"])]
+    pkt_threads = [t for t in workers if pkt_pattern.match(t["name"])]
+
+    if len(pkt_threads) > len(worker_threads):
+        print("Too many moloch workers for {} cpu threads".format(len(worker_threads)))
+        sys.exit(1)
+
+    for thread in cap_threads:
+        subprocess.call(['/usr/bin/sudo', 'taskset', '-pc', str(cap_thread), str(thread["pid"])])
+
+    for i, thread in enumerate(pkt_threads):
+        subprocess.call(['/usr/bin/sudo', 'taskset', '-pc', str(worker_threads[i]), str(thread["pid"])])
+
+    lines = []
+    with open("/proc/interrupts", "rb") as f:
+        lines = [l.decode().split(":")[0].lstrip() for l in f if CAP_IFACE in l.decode()]
+
+    if len(lines) == 0 or len(lines) > 1:
+        print("found {} irq for {}, should be 1".format(len(lines), CAP_IFACE))
+        sys.exit(1)
+
+    irq = lines[0]
+    irq = os.path.join('/proc/irq', str(irq), 'smp_affinity_list')
+
+    subprocess.Popen(['/usr/bin/sudo /bin/bash -c \'echo {} > {}\''.format(intr_thread, irq)], shell=True)
+```
+
+Finally, you can isolate specific CPU cores from kernel. Linux will no longer schedule anything on them. But you can assign any process you want there. Might be a good idea if you are not planning to run any other process on those cores anyway. Might be overkill if you are not experiencing *that* much traffic (you have to update boot loader params and reboot the machine).
+
+```
+GRUB_CMDLINE_LINUX_DEFAULT="processor.max_cstate=3 intel_idle.max_cstate=3 apparmor=0 mce=ignore_ce isolcpus=17-31"
+```
+
+## Tasks
+
+ * Isolate 2 cores from your vagrant box;
+  * Start moloch capture with 2 packet threads;
+  * Pin each worker to an isolated CPU thread;
+  * write a python script that identifies the moloch worker thread process ID-s;
+ * Fix the moloch-capture service, so it would always be bound to same NUMA node as your capture NIC (yes, I know, there is only 0 in a VM);
+ * Make sure that viewer and WISE do not steal CPU time from those workers;
+ * Make sure that your elasticsearch instance does not steal CPU time from Moloch workers (hint - grep in docker cli options);
---
+++ Moloch/advanced-config/provision.sh
@@ -0,0 +1,318 @@
+PKGDIR=/vagrant/pkgs
+WGET_PARAMS="-4 -q"
+
+DOCKER_ELA="docker.elastic.co/elasticsearch/elasticsearch-oss:6.7.0"
+MOLOCH="moloch_1.8.0-1_amd64.deb"
+USER="vagrant"
+
+if [[ -n $(ip link show | grep eth0) ]]; then
+  IFACE_EXT="eth0"
+  IFACE_INT="eth1"
+  IFACE_PATTERN="eth"
+else
+  IFACE_EXT="enp0s3"
+  IFACE_INT="enp0s8"
+  IFACE_PATTERN="enp"
+fi
+
+mkdir -p $PKGDIR
+
+if [ "$(id -u)" != "0" ]; then
+   echo "ERROR - This script must be run as root" 1>&2
+   exit 1
+fi
+
+start=$(date)
+# basic OS config
+FILE=/etc/sysctl.conf
+grep "disable_ipv6" $FILE || cat >> $FILE <<EOF
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+EOF
+grep "vm.max_map_count" $FILE || cat >> $FILE <<EOF
+vm.max_map_count=262144
+EOF
+sysctl -p
+
+echo $start >  /vagrant/provision.log
+echo 'Acquire::ForceIPv4 "true";' | sudo tee /etc/apt/apt.conf.d/99force-ipv4
+export DEBIAN_FRONTEND=noninteractive
+
+echo "Installing prerequisite packages..."
+apt-get update && apt-get -y install jq wget curl pcregrep python-minimal python-pip python3-pip python-yaml libpcre3-dev libyaml-dev uuid-dev libmagic-dev pkg-config g++ flex bison zlib1g-dev libffi-dev gettext libgeoip-dev make libjson-perl libbz2-dev libwww-perl libpng-dev xz-utils libffi-dev libsnappy-dev numactl >> /vagrant/provision.log 2>&1
+
+check_service(){
+  systemctl daemon-reload
+  systemctl is-enabled $1.service 2>/dev/null | grep "disabled" && systemctl enable $1.service
+  systemctl status $1.service | egrep  "inactive|failed" && systemctl start $1.service
+  systemctl status $1.service
+}
+install_suricata_from_ppa(){
+  add-apt-repository ppa:oisf/suricata-stable > /dev/null 2>&1 \
+  && apt-get update > /dev/null \
+  && apt-get install -y suricata > /dev/null
+}
+
+echo "Provisioning SURICATA"
+suricata -V || install_suricata_from_ppa
+pip3 install --upgrade suricata-update
+
+touch  /etc/suricata/threshold.config
+mkdir -p /var/lib/suricata/rules
+[[ -f /var/lib/suricata/rules/scirius.rules ]] || touch /etc/suricata/rules/scirius.rules
+[[ -f /var/lib/suricata/rules/suricata.rules ]] || touch /etc/suricata/rules/suricata.rules
+
+FILE=/var/lib/suricata/rules/custom.rules
+[[ -f $FILE ]] || cat > $FILE <<EOF
+alert http \$HOME_NET any -> \$EXTERNAL_NET any (msg:"CDMCS: External Windows executable download"; flow:established,to_server; content:"GET "; uricontent:".exe"; nocase; classtype:policy-violation; sid:3000001; rev:1; metadata:created_at 2018_01_19, updated_at 2018_01_19;) 
+alert dns any any -> any any (msg:"CDMCS: DNS request for Facebook"; content:"facebook"; classtype:policy-violation; sid:3000002; rev:1; metadata:created_at 2018_01_19, updated_at 2018_01_19;)
+alert tls any any -> any any (msg:"CDMCS: Facebook certificate detected"; tls.subject:"facebook"; classtype:policy-violation; sid:3000003; rev:1; metadata:created_at 2018_01_19, updated_at 2018_01_19;)
+EOF
+
+FILE=/var/lib/suricata/rules/lua.rules
+[[ -f $FILE ]] || cat > $FILE <<EOF
+alert tls any any -> any any (msg:"CDMCS TLS Self Signed Certificate"; flow:established; luajit:self-signed-cert.lua; tls.store; classtype:protocol-command-decode; sid:3000004; rev:1;)
+EOF
+
+FILE=/var/lib/suricata/rules/self-signed-cert.lua
+[[ -f $FILE ]] || cat > $FILE <<EOF
+function init (args)
+    local needs = {}
+    needs["tls"] = tostring(true)
+    return needs
+end
+function match(args)
+    version, subject, issuer, fingerprint = TlsGetCertInfo();
+    if subject == issuer then
+        return 1
+    else
+        return 0
+    end
+end
+EOF
+
+if $DEBUG ; then ip addr show; fi
+systemctl stop suricata
+pgrep Suricata || [[ -f /var/run/suricata.pid ]] && rm /var/run/suricata.pid
+
+echo "Configuring SURICATA"
+
+echo "Adding includes for SURICATA"
+FILE=/etc/suricata/suricata.yaml
+grep "cdmcs" $FILE || cat >> $FILE <<EOF
+include: /etc/suricata/cdmcs-detect.yaml
+include: /etc/suricata/cdmcs-logging.yaml
+EOF
+
+echo "Adding detects for SURICATA"
+FILE=/etc/suricata/cdmcs-detect.yaml
+grep "CDMCS" $FILE || cat >> $FILE <<EOF
+%YAML 1.1
+---
+# CDMCS
+af-packet:
+  - interface: $IFACE_EXT
+    cluster-id: 98
+    cluster-type: cluster_flow
+    defrag: yes
+default-rule-path: /var/lib/suricata/rules
+rule-files:
+ -  suricata.rules
+ -  custom.rules
+ -  lua.rules
+sensor-name: CDMCS
+EOF
+
+echo "Adding outputs for SURICATA"
+FILE=/etc/suricata/cdmcs-logging.yaml
+
+grep "CDMCS" $FILE || cat >> $FILE <<EOF
+%YAML 1.1
+---
+# CDMCS
+outputs:
+  - fast:
+      enabled: no
+      filename: fast.log
+      append: yes
+  - tls-store:
+      enabled: yes
+  - eve-log:
+      enabled: 'yes'
+      filetype: regular #regular|syslog|unix_dgram|unix_stream|redis
+      filename: eve.json
+      community-id: yes
+      community-id-seed: 0
+      types:
+        - alert:
+            payload: no             # enable dumping payload in Base64
+            payload-buffer-size: 4kb # max size of payload buffer to output in eve-log
+            payload-printable: no   # enable dumping payload in printable (lossy) format
+            packet: yes              # enable dumping of packet (without stream segments)
+            http-body: no           # enable dumping of http body in Base64
+            http-body-printable: no # enable dumping of http body in printable format
+            metadata: no             # enable inclusion of app layer metadata with alert. Default yes
+            tagged-packets: no
+EOF
+
+#if $DEBUG ; then suricata -T -vvv; fi
+[[ -f /etc/init.d/suricata ]] && rm /etc/init.d/suricata
+FILE=/etc/systemd/system/suricata.service
+grep "suricata" $FILE || cat > $FILE <<EOF
+[Unit]
+Description=suricata daemon
+After=network.target
+
+[Service]
+User=root
+Group=root
+WorkingDirectory=/var/run/
+ExecStart=/usr/bin/suricata -c /etc/suricata/suricata.yaml --pidfile /var/run/suricata.pid --af-packet -D -vvv
+Type=forking
+
+[Install]
+WantedBy=multi-user.target
+EOF
+check_service suricata || exit 1
+
+echo "Updating rules"
+suricata-update enable-source ptresearch/attackdetection
+suricata-update enable-source sslbl/ssl-fp-blacklist
+suricata-update enable-source oisf/trafficid
+suricata-update enable-source tgreen/hunting
+suricata-update list-enabled-sources
+suricata-update
+sleep 3
+suricatasc -c "reload-rules" || exit 1
+
+docker network ls | grep cdmcs >/dev/null || docker network create -d bridge cdmcs
+
+docker ps -a | grep elastic || docker run -dit --name elastic -h elastic --network cdmcs -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" --restart unless-stopped -p 9200:9200 $DOCKER_ELA 
+sleep 10
+
+echo "Provision moloch"
+cd $PKGDIR
+[[ -f $MOLOCH ]] || wget $WGET_PARAMS https://files.molo.ch/builds/ubuntu-18.04/$MOLOCH
+dpkg -s moloch || dpkg -i $MOLOCH
+
+echo "Configuring interfaces"
+for iface in ${ifaces//;/ }; do
+  echo "Setting capture params for $iface"
+  for i in rx tx tso gso gro tx nocache copy sg rxvlan; do ethtool -K $iface $i off > /dev/null 2>&1; done
+done
+
+echo "Configuring moloch"
+delim=";"; ifaces=""; for item in `ls /sys/class/net/ | egrep '^eth|ens|eno|enp'`; do ifaces+="$item$delim"; done ; ifaces=${ifaces%"$deli$delim"}
+cd /data/moloch/etc
+FILE=/data/moloch/etc/config.ini
+[[ -f config.ini ]] || cp config.ini.sample $FILE
+sed -i "s/MOLOCH_ELASTICSEARCH/localhost:9200/g"  config.ini
+sed -i "s/MOLOCH_INTERFACE/$ifaces/g"             config.ini
+sed -i "s/MOLOCH_INSTALL_DIR/\/data\/moloch/g"    config.ini
+sed -i "s/MOLOCH_INSTALL_DIR/\/data\/moloch/g"    config.ini
+sed -i "s/MOLOCH_PASSWORD/test123/g"              config.ini
+
+echo "Configuring capture plugins"
+sed -i -e 's,#wiseHost=127.0.0.1,wiseHost=127.0.0.1\nwiseCacheSecs=60\nplugins=wise.so\nviewerPlugins=wise.js\nwiseTcpTupleLookups=true\nwiseUdpTupleLookups=true\n,g' config.ini
+
+cp wise.ini.sample wiseService.ini
+grep CDMCS wiseService.ini || cat >> wiseService.ini <<EOF
+# CDMCS
+[reversedns]
+ips=10.0.0.0/8
+field=asset
+EOF
+
+echo "Configuring databases"
+cd /data/moloch/db
+if [[ `./db.pl localhost:9200 info | grep "DB Version" | cut -d ":" -f2 | tr -d " "` -eq -1 ]]; then
+  echo "INIT" | ./db.pl localhost:9200 init
+fi
+
+cd /data/moloch/bin
+./moloch_update_geo.sh > /dev/null 2>&1
+chown nobody:daemon /data/moloch/raw
+
+echo "Adding viewer user"
+cd /data/moloch/viewer && ../bin/node addUser.js vagrant vagrant vagrant --admin
+
+echo "Configuring system limits"
+ulimit -l unlimited
+grep memlock /etc/security/limits.conf || echo "nofile 128000 - memlock unlimited" >> /etc/security/limits.conf
+mkdir /data/moloch/raw && chown nobody:daemon /data/moloch/raw
+
+echo "Configuring systemd services"
+
+FILE=/etc/systemd/system/moloch-wise.service
+grep "moloch-wise" $FILE || cat > $FILE <<EOF
+[Unit]
+Description=Moloch WISE
+After=network.target
+
+[Service]
+Type=simple
+Restart=on-failure
+ExecStart=/data/moloch/bin/node wiseService.js -c /data/moloch/etc/wiseService.ini
+WorkingDirectory=/data/moloch/wiseService
+SyslogIdentifier=moloch-wise
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+FILE=/etc/systemd/system/moloch-viewer.service
+grep "moloch-viewer" $FILE || cat > $FILE <<EOF
+[Unit]
+Description=Moloch Viewer
+After=network.target moloch-wise.service
+
+[Service]
+Type=simple
+Restart=on-failure
+ExecStart=/data/moloch/bin/node viewer.js -c /data/moloch/etc/config.ini
+WorkingDirectory=/data/moloch/viewer
+SyslogIdentifier=moloch-viewer
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+FILE=/etc/systemd/system/moloch-capture.service
+grep "moloch-capture" $FILE || cat > $FILE <<EOF
+PIDFILE=/var/run/capture.pid
+[Unit]
+Description=Moloch Capture
+After=network.target moloch-wise.service moloch-viewer.service
+
+[Service]
+Type=simple
+Restart=on-failure
+#ExecStartPre=-/data/moloch/bin/start-capture-interfaces.sh
+ExecStart=/data/moloch/bin/moloch-capture -c /data/moloch/etc/config.ini --host $(hostname)
+WorkingDirectory=/data/moloch
+LimitCORE=infinity
+LimitMEMLOCK=infinity
+SyslogIdentifier=moloch-capture
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+for service in viewer capture ; do
+  systemctl enable moloch-$service.service
+  systemctl start moloch-$service.service
+  systemctl status moloch-$service.service
+done
+
+mkdir -p /home/vagrant/.local/bin && chown -R vagrant /home/vagrant/.local
+su - vagrant -c "pip3 install --user --upgrade psutil"
+
+echo "making some noise"
+while : ; do curl -s https://www.facebook.com/ > /dev/null 2>&1 ; sleep $(shuf -i 15-60 -n 1); done &
+while : ; do curl -s https://sysadminnid.tumblr.com/ > /dev/null 2>&1 ; sleep $(shuf -i 15-60 -n 1); done &
+while : ; do curl -s http://testmyids.com > /dev/null 2>&1 ; sleep $(shuf -i 15-60 -n 1); done &
+while : ; do curl -s -k https://self-signed.badssl.com/ > /dev/null 2>&1 ; sleep $(shuf -i 15-60 -n 1); done &
+while : ; do dig NS berylia.org @1.1.1.1 > /dev/null 2>&1 ; sleep $(shuf -i 15-60 -n 1); done &
+while : ; do dig NS berylia.org @8.8.8.8 > /dev/null 2>&1 ; sleep $(shuf -i 15-60 -n 1); done &
